bd0d22296b7df6aad67d38c10875189c62d8c57f
Chen Anxue <46052474+cascades-sjtu@users.noreply.github.com>
2022-05-14 08:40:07 +0800

    change  mat

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import taichi as ti
 from taichi.math import vec3
 
 scene = Scene(exposure=10)
-scene.set_floor(-0.05, (1.0, 1.0, 1.0))
+scene.set_floor(-1, (1.0, 1.0, 1.0))
 scene.set_background_color((1.0, 0, 0))
 
 
@@ -11,7 +11,7 @@ scene.set_background_color((1.0, 0, 0))
 def sphere(pos, r, mat, color):
     for i, j, k in ti.ndrange((-64, 64), (-64, 64), (-64, 64)):
         if (i - pos[0])**2 + (j - pos[1])**2 + (k - pos[2])**2 < r * r:
-            scene.set_voxel(vec3(i, j, k), mat, (1, 1, 1))
+            scene.set_voxel(vec3(i, j, k), mat, color)
 
 
 @ti.kernel
@@ -19,7 +19,7 @@ def initialize_voxels():
     # Your code here! :-)
     pos = vec3(0, 0, 0)
     color = vec3(213. / 255, 255. / 255, 13. / 255)
-    sphere(pos, 15, 1, color)
+    sphere(pos, 15, 2, color)
 
 
 initialize_voxels()
