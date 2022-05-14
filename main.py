7726a63de3928a855b72b397610ec1721672c989
Chen Anxue <46052474+cascades-sjtu@users.noreply.github.com>
2022-05-14 09:24:14 +0800

    a neat sphere

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from scene import Scene
 import taichi as ti
-from taichi.math import vec3
+from taichi.math import vec3, ivec3
 
-scene = Scene(exposure=10)
-scene.set_floor(-1, (1.0, 1.0, 1.0))
-scene.set_background_color((1.0, 0, 0))
+scene = Scene(exposure=1)
+scene.set_floor(-1, (0.8, 0.8, 0.8))
+scene.set_directional_light((0, 1, 0), 0.2, (1, 1, 1))
+scene.set_background_color((0.8, 0.8, 0.8))
 
 
 @ti.func
 def sphere(pos, r, mat, color):
     for i, j, k in ti.ndrange((-64, 64), (-64, 64), (-64, 64)):
-        if (i - pos[0])**2 + (j - pos[1])**2 + (k - pos[2])**2 < r * r:
+        x = ivec3(i - pos[0], j - pos[1], k - pos[2])
+        if x.dot(x) < r * r:
             scene.set_voxel(vec3(i, j, k), mat, color)
 
 
@@ -19,7 +21,7 @@ def initialize_voxels():
     # Your code here! :-)
     pos = vec3(0, 0, 0)
     color = vec3(213. / 255, 255. / 255, 13. / 255)
-    sphere(pos, 15, 2, color)
+    sphere(pos, 10, 1, color)
 
 
 initialize_voxels()
